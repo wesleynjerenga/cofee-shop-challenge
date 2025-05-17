c041193617454f27d59096be9e1e8e8e2c63b072
Wesley <wesleynjerenga9@gmail.com>
2025-05-17 09:37:04 +0300

    adding and working on the custome r file

--- coffee-shop-challenge/customer.py
+++ coffee-shop-challenge/customer.py
@@ -1,16 +1,40 @@
 class Customer:
-    def __init__(self, name, email):
+    _all = []
+
+    def __init__(self, name):
         self.name = name
-        self.email = email
+        Customer._all.append(self)
 
-    def update_info(self, name=None, email=None):
-        if name:
-            self.name = name
-        if email:
-            self.email = email
+    @property
+    def name(self):
+        return self._name
 
-    def get_info(self):
-        return {
-            "name": self.name,
-            "email": self.email
-        }
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str):
+            raise TypeError("Name must be a string.")
+        if not (1 <= len(value) <= 15):
+            raise ValueError("Name must be 1–15 characters.")
+        self._name = value
+
+    def orders(self):
+        from order import Order
+        return [order for order in Order._all if order.customer is self]
+
+    def coffees(self):
+        return list({order.coffee for order in self.orders()})
+
+    def create_order(self, coffee, price):
+        from order import Order
+        return Order(self, coffee, price)
+
+    @classmethod
+    def most_aficionado(cls, coffee):
+        from order import Order
+        spending = {}
+        for order in Order._all:
+            if order.coffee is coffee:
+                spending[order.customer] = spending.get(order.customer, 0) + order.price
+        if not spending:
+            return None
+        return max(spending, key=spending.get)
